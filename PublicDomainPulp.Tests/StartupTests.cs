bc5d9bccde28f520d4af1a706c5ca89e9f7ef542
John Q. Pulp <pulp@publicdomainpulp.com>
2025-12-14 11:46:38 -0600

    Improve endpoints, including caching for files

--- PublicDomainPulp.Tests/StartupTests.cs
+++ PublicDomainPulp.Tests/StartupTests.cs
@@ -1,3 +1,5 @@
+using System.Net;
+
 namespace Pulp.PublicDomainPulp.Tests;
 
 [TestClass]
@@ -8,5 +10,35 @@ public class StartupTests {
 		res.EnsureSuccessStatusCode();
 		res.AssertContentType("text/html", true);
 		await res.AssertHasTitle();
+	}
+	
+	[TestMethod]
+	public async Task VN_200() {
+		HttpResponseMessage res = await TestApp.Client.GetAsync("/vn/cupofgold/pulp.html");
+		res.EnsureSuccessStatusCode();
+		res.AssertContentType("text/html", true);
+		await res.AssertHasTitle();
+	}
+	
+	[TestMethod]
+	[DataRow("b-title.webp")]
+	[DataRow("o-pearl.webp")]
+	[DataRow("c-la.webp")]
+	public async Task VN_Image_200(string image) {
+		HttpResponseMessage res = await TestApp.Client.GetAsync("/vn/cupofgold/images/" + image);
+		res.EnsureSuccessStatusCode();
+		res.AssertContentType("image/webp", false);
+
+		string cacheControl = res.Headers.CacheControl.ToString();
+		Assert.AreEqual($"public, max-age={60 * 60 * 24 * 7}", cacheControl);
+	}
+	
+	[TestMethod]
+	[DataRow("b-foo.webp")]
+	[DataRow("o-foo.webp")]
+	[DataRow("c-foo.webp")]
+	public async Task VN_Image_404(string image) {
+		HttpResponseMessage res = await TestApp.Client.GetAsync("/vn/cupofgold/images/" + image);
+		Assert.AreEqual(HttpStatusCode.NotFound, res.StatusCode);
 	}
 }
--- PublicDomainPulp/Program.cs
+++ PublicDomainPulp/Program.cs
@@ -1,3 +1,5 @@
+using System.Net;
+using System.Net.Http.Headers;
 using System.Text;
 using Microsoft.Extensions.FileProviders;
 using Microsoft.Net.Http.Headers;
@@ -37,12 +39,12 @@ app.UseStaticFiles(new StaticFileOptions
 app.MapGet("/", () => {
 	string html = Helpers.HeadHtml;
 	html += "PublicDomainPulp is a website for hosting visual novel transformation (pulpifications) of public domain (and creative commons) fiction books. The current catalog:<br><ul>";
-	html += string.Join("<br>", visualPulps.Values.Select(vp => $"<li><a href='/vn/{vp.DirName}/'>{vp.DirName}</a></li>"));
+	html += string.Join("<br>", visualPulps.Values.Select(vp => $"<li><a href='/vn/{vp.DirName}/pulp.html'>{vp.DirName}</a></li>"));
 	html += "</ul>";
 	return Results.Text(html, "text/html; charset=utf-8", Encoding.UTF8, 200);
 });
 
-app.MapGet("/vn/{book}/", (string book) =>
+app.MapGet("/vn/{book}/pulp.html", (string book) =>
 {
 	if (!visualPulps.TryGetValue(book, out VisualPulp pulp)) {
 		return Results.NotFound();
@@ -50,15 +52,25 @@ app.MapGet("/vn/{book}/", (string book) =>
 	return Results.Text(pulp.Html, "text/html; charset=utf-8", Encoding.UTF8, 200);
 });
 
-app.MapGet("/vn/{book}/images/{image}.webp", (string book, string image) =>
+app.MapGet("/vn/{book}/images/{image}.webp", async (HttpContext context) =>
 {
-	if (!visualPulps.TryGetValue(book, out VisualPulp pulp)) {
-		return Results.NotFound();
+	string book = context.Request.RouteValues["book"] as string;
+	string image = context.Request.RouteValues["image"] as string;
+
+	if (visualPulps.TryGetValue(book, out VisualPulp pulp)) {
+		context.Response.ContentType = "image/webp";
+		string path = Path.Combine(baseDirectory, "VisualPulps", pulp.DirName, "images", image + ".webp");
+
+		try {
+			context.Response.StatusCode = 200;
+			context.Response.Headers.CacheControl = "public, max-age=604800";
+			await context.Response.SendFileAsync(path);
+			return Results.Empty;
+		} catch (FileNotFoundException) { }
 	}
 
-	string path = Path.Combine(baseDirectory, "VisualPulps", pulp.DirName, "images", image + ".webp");
-
-	return Results.File(path, "image/webp");
+	context.Response.StatusCode = 404;
+	return Results.Empty;
 });
 
 app.Run();
