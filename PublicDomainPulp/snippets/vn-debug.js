175710f2d79d6f38d203b6fd51cfd3e493b3e31b
John Q. Pulp <pulp@publicdomainpulp.com>
2026-04-12 21:28:49 -0500

    Clipboard copying

--- PublicDomainPulp/snippets/vn-debug.js
+++ PublicDomainPulp/snippets/vn-debug.js
@@ -1,11 +1,13 @@
 document.addEventListener("keydown", e => {
   if (e.key === "," || e.key === ".") {
     const increment = e.key === "," ? -1 : 1;
-    const speakerElem = document.querySelector("#pulp:nth-child(2) .speaker-back")
-    const matchArr = speakerElem.style.backgroundImage.match(/(c-[^-\.]+(-a[^-\.]+)?((-x[^-\.]+)+)?)(-e[^-\.]+)?(-s?[123]?)?/)
+    const speakerElem = document.querySelector("#pulp:nth-child(2) .speaker-back");
+    const regex = /(c-([^-\.]+)(-a[^-\.]+)?((-x[^-\.]+)+)?)(-e([^-\.]+))?(-s?([123])?)?/;
+    const matchArr = speakerElem.style.backgroundImage.match(regex);
     const expressionsArr = expressions[matchArr[1]]
     const expressionNew = expressionsArr[(expressionsArr.indexOf(matchArr[0]) + increment + expressionsArr.length) % expressionsArr.length]
-    console.log(expressionNew)
-    speakerElem.outerHTML = `<div class="speaker-back" style="background-image: url(images/${expressionNew}.${imageExt})"></div>`
+    speakerElem.outerHTML = `<div class="speaker-back" style="background-image: url(images/${expressionNew}.${imageExt})"></div>`;
+    const matchArr2 = expressionNew.match(regex);
+    navigator.clipboard.writeText("e:" + matchArr2[2] + "=" + (matchArr2[7] ?? "") + ";i=" + (matchArr2[9] ?? ""));
   }
 });
