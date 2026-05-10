e96473b71a9a554fa70646834dab49aa04a6cf6d
John Q. Pulp <pulp@publicdomainpulp.com>
2026-05-10 15:57:58 -0500

    Use wasd in editing

--- PublicDomainPulp/snippets/vn-debug.js
+++ PublicDomainPulp/snippets/vn-debug.js
@@ -1,6 +1,6 @@
 document.addEventListener("keydown", e => {
-  if (e.key === "," || e.key === ".") {
-    const increment = e.key === "," ? -1 : 1;
+  if (e.key === "w" || e.key === "s") {
+    const increment = e.key === "s" ? -1 : 1;
     const speakerElem = document.querySelector("#pulp:nth-child(2) .speaker-back");
     const regex = /(c-([^-\.]+)(-a[^-\.]+)?((-x[^-\.]+)+)?)(-e([^-\.]+))?(-s?([123])?)?/;
     const matchArr = speakerElem.style.backgroundImage.match(regex);
