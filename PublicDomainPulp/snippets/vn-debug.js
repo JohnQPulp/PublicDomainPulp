6270110938335dbed0b5b4fddc7933b24c9f8a5b
John Q. Pulp <pulp@publicdomainpulp.com>
2026-05-13 16:51:14 -0500

    Easier expression spinning

--- PublicDomainPulp/snippets/vn-debug.js
+++ PublicDomainPulp/snippets/vn-debug.js
@@ -1,14 +1,22 @@
 document.addEventListener("keydown", e => {
   if (e.key === "w" || e.key === "s") {
-    const increment = e.key === "s" ? -1 : 1;
+    const increment = e.key === "w" ? -1 : 1;
     const speakerElem = document.querySelector("#pulp:nth-child(2) .speaker-back");
-    const regex = /(c-([^-\.]+)(-a[^-\.]+)?((-x[^-\.]+)+)?)(-e([^-\.]+))?(-s?([123])?)?/;
-    const matchArr = speakerElem.style.backgroundImage.match(regex);
-    const expressionsArr = expressions[matchArr[1]]
-    const expressionNew = expressionsArr[(expressionsArr.indexOf(matchArr[0]) + increment + expressionsArr.length) % expressionsArr.length]
-    speakerElem.outerHTML = `<div class="speaker-back" style="background-image: url(images/${expressionNew}.${imageExt})"></div>`;
-    const matchArr2 = expressionNew.match(regex);
-    navigator.clipboard.writeText(";e:" + matchArr2[2] + "=" + (matchArr2[7] ?? "") + ";i=" + (matchArr2[9] ?? ""));
+    if (speakerElem) {
+      const regex = /(c-([^-\.]+)(-a[^-\.]+)?((-x[^-\.]+)+)?)(-e([^-\.]+))?(-s?([123])?)?/;
+      const matchArr = speakerElem.style.backgroundImage.match(regex);
+      const expressionsArr = expressions[matchArr[1]];
+      let expressionOld = matchArr[0];
+      let expressionNew;
+      let index = expressionsArr.indexOf(expressionOld) + expressionsArr.length;
+      do {
+        index += increment;
+        expressionNew = expressionsArr[index % expressionsArr.length];
+      } while (expressionOld.includes("-s") !== expressionNew.includes("-s"));
+      speakerElem.outerHTML = `<div class="speaker-back" style="background-image: url(images/${expressionNew}.${imageExt})"></div>`;
+      const matchArr2 = expressionNew.match(regex);
+      navigator.clipboard.writeText(";e:" + matchArr2[2] + "=" + (matchArr2[7] ?? "") + ";i=" + (matchArr2[9] ?? ""));
+    }
   }
 });
 toggleEditorializing();
